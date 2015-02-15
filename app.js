355c0e2387051c6e9a9e9119d630b397638373a4
Connor Claflin <cclaflin33@gmail.com>
2015-02-15 17:53:53 -0500

    streamlining step complete

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 var express = require('express');
 var app = express();
+var morgan = require('morgan');
 
 app.use(morgan('dev'));
+
+app.get('/', function (req, res) {
+  res.send('Hello Mr. Anderson!')
+})
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('app listening at http://%s:%s', host, port)
+
+})
