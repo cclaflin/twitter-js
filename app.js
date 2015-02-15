2ce2726d63d0eba16708283c7a1984de4c3b1eaa
Connor Claflin <cclaflin33@gmail.com>
2015-02-15 18:22:25 -0500

    Rendering Template step

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var app = express();
 var morgan = require('morgan');
+var swig = require('swig');
 
 app.use(morgan('dev'));
 
 app.get('/', function (req, res) {
-  res.send('Hello Mr. Anderson!')
+  //res.send('Hello Mr. Anderson!')
+  var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+  res.render( 'index', {title: 'Hall of Fame', people: people} );
 })
+
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+swig.setDefaults({ cache: false });
 
 var server = app.listen(3000, function () {
 
